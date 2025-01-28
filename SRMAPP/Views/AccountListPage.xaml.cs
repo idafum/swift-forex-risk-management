3c7f90bbe155ec792c4487d73e21890aab86a487
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-01-28 15:18:32 -0600

    add swipe feature to account list page

--- SRMAPP/Views/AccountListPage.xaml.cs
+++ SRMAPP/Views/AccountListPage.xaml.cs
@@ -3,8 +3,20 @@ using SRMAPP.ViewModels;
 namespace SRMAPP.Views;
 public partial class AccountListPage : ContentPage
 {
-    public AccountsPage()
+    public AccountListPage()
     {
         InitializeComponent();
     }
+
+    private void OnDeleteSwipeItemInvoked(object sender, EventArgs eventArgs)
+    {
+
+    }
+
+    private void OnTradeSwipeItemInvoked(object sender, EventArgs eventArgs)
+    {
+
+    }
+
+
 }
