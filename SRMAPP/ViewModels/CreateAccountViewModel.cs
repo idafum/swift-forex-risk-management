c7836dbafd56cc638959d6db2bfca8df22617c68
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-01-30 11:30:02 -0600

    define CreateAccountViewModel

---
+++ SRMAPP/ViewModels/CreateAccountViewModel.cs
@@ -0,0 +1,13 @@
+/* This Viewmodel combines the Create Account View and the Account model
+    It creates a new account object and returs that object to the calling viewmodel
+*/
+namespace SRMAPP.ViewModels;
+
+public class CreateAccountViewModel
+{
+
+    public CreateAccountViewModel()
+    {
+
+    }
+}
