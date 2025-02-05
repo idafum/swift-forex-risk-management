ef9a1fff07144623a7a1b25d9b45fbdbed16b258
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-02-05 01:50:28 -0600

    UI for login and signup, navigation from login to signup

---
+++ Risk2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Risk2.Views;
+
+namespace Risk2.ViewModels;
+
+public partial class LoginViewModel : ObservableObject
+{
+
+    public LoginViewModel()
+    {
+
+    }
+
+}
--- Risk2/Views/LoginPage.xaml.cs
+++ Risk2/Views/LoginPage.xaml.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using Risk2.ViewModels;
+
 namespace Risk2.Views;
 
 public partial class LoginPage : ContentPage
@@ -5,5 +8,21 @@ public partial class LoginPage : ContentPage
     public LoginPage()
     {
         InitializeComponent();
+        BindingContext = new LoginViewModel();
+    }
+
+    void OnTapGestureRecognizerTapped(object sender, TappedEventArgs args)
+    {
+        //Change text color
+        Label? label = sender as Label;
+
+        if (label != null)
+            label.TextColor = Colors.Blue;
+
+
+        if (Application.Current?.Windows.Count > 0)
+        {
+            Application.Current.Windows[0].Page = new SignUpPage();
+        }
     }
 }
