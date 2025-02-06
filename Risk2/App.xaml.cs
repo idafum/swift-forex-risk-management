af857f5440b15f0b24d304884921fbd0e35114df
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-02-05 20:19:30 -0600

    Inject the user repository in the app constructor

--- Risk2/App.xaml.cs
+++ Risk2/App.xaml.cs
@@ -1,12 +1,15 @@
-﻿using Risk2.Views;
+﻿using Risk2.Data.Repositories;
+using Risk2.Views;
 
 namespace Risk2;
 
 public partial class App : Application
 {
-	public App()
+	public static UserRepository? UserRepo { get; private set; }
+	public App(UserRepository repo) //DI automatically populated the repo parameter to the constructor
 	{
 		InitializeComponent();
+		UserRepo = repo;
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
